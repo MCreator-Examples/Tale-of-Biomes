973311f32aed9354f0109bd31b7d7b7f75241657
NorthWestTrees <northwesttreesgaming@gmail.com>
2024-03-05 12:52:20 -0800

    r0.3.1-Indev Patch
    Fixed critical issues with Gradle that would prevent the test environment from loading.

--- src/main/java/net/nwtg/taleofbiomes/procedures/PiruffTreeAdditionalGenerationConditionProcedure.java
+++
@@ -1,9 +0,0 @@
-package net.nwtg.taleofbiomes.procedures;
-
-import net.minecraft.world.level.LevelAccessor;
-
-public class PiruffTreeAdditionalGenerationConditionProcedure {
-	public static boolean execute(LevelAccessor world, double x, double y, double z) {
-		return PiruffTreeStructureScriptProcedure.execute(world, x, y, z);
-	}
-}
